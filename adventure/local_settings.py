4d9d306e5994e6bdbd7853d664a9864041b31231
DESKTOP1\Lokesh Johri <Lokesh.Johri@tantiv4.com>
2017-11-26 18:50:21 -0800

    adding the adventire/alohomora files

---
+++ adventure/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'adventure',
+        'USER': 'postgres',
+        'PASSWORD': 'hakunamatata',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
